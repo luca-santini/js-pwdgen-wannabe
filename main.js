8d3bb9419e2758bc9e87413e48a8290d5fc5560e
Luca Santini <lucasantinidev@gmail.com>
2020-04-17 16:07:48 +0200

    concatenazione elementi in pagina

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 // chiedere nome all'utente
-var nome_utente = prompt("Qual' é il tuo nome"("Inserisci il tuo nome"));
+var nome_utente = prompt("Inserisci il tuo nome", "Luca");
 // chiedere il cognome all'utente
-var cognome_utente = prompt("Qual'è il tuo cognome"("Inserisci il tuo cognome"));
+var cognome_utente = prompt("Inserisci il tuo cognome", 'Santini');
 // chiedere il colore preferito all'utente
-var colore_utente = prompt("Qual'è il tuo colore preferito"("Inserisci il tuo colore preferito"));
+var colore_utente = prompt("Inserisci il tuo colore preferito", 'blu');
+// numero
+var numero = ("20");
+
+document.getElementById("password").innerHTML = nome_utente + cognome_utente + colore_utente + numero;
