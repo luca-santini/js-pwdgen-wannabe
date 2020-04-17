c9760d78b548f1c2eabbc519be6390ec674dab63
Luca Santini <lucasantinidev@gmail.com>
2020-04-17 15:29:51 +0200

    3 variabili
    var nome_utente
    var cognome_utente
    var colore_utente

---
+++ main.js
@@ -0,0 +1,6 @@
+// chiedere nome all'utente
+var nome_utente = prompt("Qual' é il tuo nome"("Inserisci il tuo nome"));
+// chiedere il cognome all'utente
+var cognome_utente = prompt("Qual'è il tuo cognome"("Inserisci il tuo cognome"));
+// chiedere il colore preferito all'utente
+var colore_utente = prompt("Qual'è il tuo colore preferito"("Inserisci il tuo colore preferito"));
