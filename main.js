09c79e42dd40ba2267277de508f89a0595866f53
Luca Santini <lucasantinidev@gmail.com>
2020-04-20 09:54:58 +0200

    console log

--- main.js
+++ main.js
@@ -3,8 +3,12 @@ var nome_utente = prompt("Inserisci il tuo nome", "Luca");
 // chiedere il cognome all'utente
 var cognome_utente = prompt("Inserisci il tuo cognome", 'Santini');
 // chiedere il colore preferito all'utente
-var colore_utente = prompt("Inserisci il tuo colore preferito", 'blu');
+var colore_utente = prompt("Inserisci il tuo colore preferito", 'Blu');
 // numero
 var numero = ("20");
 
 document.getElementById("password").innerHTML = nome_utente + cognome_utente + colore_utente + numero;
+
+console.log(nome_utente);
+console.log(cognome_utente);
+console.log(colore_utente);
